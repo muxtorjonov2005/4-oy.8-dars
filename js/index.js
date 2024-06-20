0ca215b72e65e23bf23b2c14add85bf284b24652
muxtorjonov2005 <muxtorjonoveldiyor@gmail.com>
2024-06-20 23:38:34 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,215 @@
+// 1-masala
+// function creatColors(color1, color2, color3){
+//     let result = [color1, color2, color3]
+//     return result
+// }
+// console.log(creatColors('qizil', 'yashil', 'kok'));
+
+// 2-masala
+// function creatNumbers(num1, num2, num3){
+//     let res = [num1, num2, num3]
+//     return res
+// }
+// console.log(creatNumbers(2,5,8));
+
+// 3-masala
+// function firstAndLast(arr){
+//     return arr
+// }
+// let arr = [2, 5, 8, 4]
+// console.log(arr[0],arr[arr.length-1]);
+
+// 4-masala
+// function secondElement(arr,element){
+//     arr[1] = element
+//     return arr
+// }
+// console.log(secondElement([1, 2, 5, 8], 3));
+
+// 5-masala
+// function addElement(arr,element){
+//     arr.push(element)
+//     return arr
+// }
+// console.log(addElement([1, 5, 8, 11], 15));
+
+// 6-masala
+// function deletLast(arr){
+//     arr.pop()
+//     return arr
+// }
+// console.log(deletLast([1,5,8,7]));
+
+// 7-masala
+// function lenghtArray(arr){
+//     return arr.length
+// }
+// console.log(lenghtArray([1, 2, 5, 10]));
+
+// 8-masala
+// function addFirst(arr,element){
+//     arr.unshift(element)
+//     return arr
+// }
+// console.log(addFirst([1,2,6,5],14));
+
+// 9-masala
+// function deletFirst(arr){
+//     arr.shift()
+//     return arr
+// }
+// console.log(deletFirst([1,2,5,8]));
+
+// 10-masala
+// function concatMassiv(arr1,arr2){
+//     let res = arr1.concat(arr2)
+//     return res
+// }
+// console.log(concatMassiv([1, 5, 8],[4, 8,]));
+
+// 11-masala
+// function checkMassiv(arr,element){
+//     let res = arr.includes(element)
+//     return res   
+// }
+// console.log(checkMassiv([1, 5, 8, 10],12));
+
+// 12-masala
+// function sortArray(arr){
+//     arr.sort()
+//     return arr
+// }
+// console.log(sortArray(['Anar', 'shaftoli', 'limon', 5]));
+
+// 13-masala
+// function findArray(arr,element){
+//     let res = arr.indexOf(element)
+//     return res
+// }
+// console.log(findArray([2,5,8],7));
+
+// 14-masala
+// function deletIndexOf(arr){
+
+// }
+
+// 15-masala
+// function deletTwoLast(arr){
+//     arr.pop()
+//     arr.pop()
+//     return arr
+// }
+// console.log(deletTwoLast([1, 8, 8, 9, 4, 5]));
+
+// 16-masala
+// function addTwoElement(arr,element1,element2){
+//     arr.unshift(element1)
+//     arr.push(element2)
+//     return arr
+// }
+// console.log(addTwoElement([1, 5, 8, 4], 5, 8));
+
+// 17-masala
+// function changeArray(arr){
+//     let first = arr[0]
+//     let last = arr[arr.lenght - 1]
+//     arr[0] = last
+//     arr[arr.lenght - 1] = first
+//     return arr
+// }
+// console.log(changeArray([1, 4, 5, 8, 40]));
+
+// 18-masala
+// function clienMassiv(arr){
+//     return []
+// }
+// console.log(clienMassiv([1, 5, 8]));
+
+// // 19-masala
+// function copyArray(arr){
+//     return arr.slice()
+// }
+// console.log(copyArray([1, 2, 5])); chalaa
+
+// 20-masala
+// function returnArray(arr){
+//     let res = []
+//     for (let i = 0; i < arr.length; i++){
+//         res.push(arr[i],arr[i])
+//         return res
+//     }
+// }
+// console.log(returnArray([1, 5, 8, 7]));
+
+// 21-masala
+// function mostBigArray(arr){
+//     return Math.max(...arr)
+// }
+// console.log(mostBigArray([1, 5, 7, 11]));
+
+// 22-masala
+// function mostSmallArray(arr){
+//     return Math.min(...arr)
+// }
+// console.log(mostSmallArray([2, 5, 8, 3]));
+
+// 23-masala
+
+// 24-masala
+// function returnArray(arr){
+//     return arr.reverse()
+// }
+// console.log(returnArray([1, 4, 5, 7]));
+
+// 25-masala
+// function filterArray(arr){
+//     return arr.filter(element => element > 0)
+// }
+// console.log(filterArray([-1, 5, -8]));
+
+// 26-masala
+// function kopaytirish(arr){
+//     return arr.map(element => element * 2)
+// }
+// console.log(kopaytirish([1, 2, 5, 8]));
+
+// 27-masala
+// function kvArray(arr){
+//     return arr.map(element => element * element)
+// }
+// console.log(kvArray([2, 4, 5, 6]));
+
+// 28-masala
+// function deletElement(arr,element){
+//     return arr.filter(e => e !== element)
+// }
+// console.log(deletElement([1, 5, 8, 7],7));
+
+// 29-masala
+// function checkElement(arr,element){
+//     return arr.includes(element)
+// }
+// console.log(checkElement([4, 5, 8, 41],4));
+
+// 30-masala
+// function sumArray(arr){
+//     return arr.reduce((a, b) => a + b, 0)
+// }
+// console.log(sumArray([1, 5, 8, 2]));
+
+// 32-masala
+
+// 33-masala
+// function concatArrays(arr1, arr2){
+//     let res = arr1.concat(arr2)
+//     return res
+// }
+// console.log(concatArrays([1, 5, 8],[4, 8, 9]));
+
+// 34-masala
+
+// 35-masala
+function array(arr){
+    return arr.sort((a, b) => a - b)
+}
+console.log(array([1,8,9,7,]));
